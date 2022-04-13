b50d9c884a6126258f410b2fd634395af5c420ee
AlexUAKH <sashaos1404@gmail.com>
2022-04-13 16:00:17 +0300

    added MongoDb

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   try {
+    const port = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(port, () => console.log(`Server runs on port ${port}`));
   } catch (e) {
     console.log(e);
   }
