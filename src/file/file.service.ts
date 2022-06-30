b9632f54816ed20e442a02325e00de702523ba9b
AlexUAKH <sashaos1404@gmail.com>
2022-06-30 15:58:50 +0300

    create file upload module

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import * as path from "path";
+import * as fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+export enum fileType {
+  AUDIO = "audio",
+  PICTURE = "picture"
+}
 
 @Injectable()
-export class FileService {}
+export class FileService {
+
+  createFile(type: fileType, file): string {
+    try {
+      const fileExtention = file.originalname.split('.').pop();
+      const fileName = uuidv4() + '.' + fileExtention;
+      console.log("fileName: ", fileName)
+      const filePath = path.resolve(__dirname, '..', 'static', type);
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, {recursive: true});
+      }
+      fs.writeFileSync(path.resolve(filePath, fileName), file.buffer);
+      return `${type}/${fileName}`;
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  removeFile(file) {
+    if (file) {
+      try {
+        const filePath = path.resolve(__dirname, '..', 'static', file);
+        console.log("file: ", file);
+        console.log("filePath: ", filePath);
+        if (filePath) {
+          fs.unlinkSync(filePath);
+        }
+      } catch (e) {
+        throw  new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+  }
+}
