9de9d439c8f827629db3e795c770f4b9ddac8323
AlexUAKH <sashaos1404@gmail.com>
2022-07-02 16:07:49 +0300

    add creat comment and file upload

--- src/song/song.controller.ts
+++ src/song/song.controller.ts
@@ -6,19 +6,27 @@ import {
   Query,
   Patch,
   Param,
-  Delete
+  Delete, UploadedFiles, UseInterceptors
 } from '@nestjs/common';
 import { SongService } from './song.service';
 import { CreateSongDto } from './dto/create-song.dto';
 import { UpdateSongDto } from './dto/update-song.dto';
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { ObjectId } from 'mongoose';
+import {FileFieldsInterceptor} from "@nestjs/platform-express";
 
-@Controller('song')
+@Controller('api/song')
 export class SongController {
   constructor(private readonly songService: SongService) {}
 
   @Post()
-  create(@Body() createSongDto: CreateSongDto) {
-    return this.songService.create(createSongDto);
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'picture', maxCount: 1 },
+    { name: 'audio', maxCount: 1 },
+  ]))
+  create(@UploadedFiles() files, @Body() createSongDto: CreateSongDto) {
+    const {picture, audio} = files;
+    return this.songService.create(createSongDto, picture[0], audio[0]);
   }
 
   @Get()
@@ -27,17 +35,27 @@ export class SongController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: ObjectId) {
     return this.songService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSongDto: UpdateSongDto) {
+  update(@Param('id') id: ObjectId, @Body() updateSongDto: UpdateSongDto) {
     return this.songService.update(id, updateSongDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: ObjectId) {
     return this.songService.remove(id);
+  }
+
+  @Post('comment')
+  creatComment(@Body() dto: CreateCommentDto) {
+    return this.songService.creatComment(dto);
+  }
+
+  @Post('listens/:id')
+  listens(@Param('id') id: ObjectId) {
+    return this.songService.listens(id);
   }
 }
