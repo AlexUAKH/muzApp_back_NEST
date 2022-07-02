fdf5b6f582f252b7e2fcd155c7564c195fd2a668
AlexUAKH <sashaos1404@gmail.com>
2022-07-02 16:04:51 +0300

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import * as path from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SongModule } from './song/song.module';
 import { FileModule } from './file/file.module';
@@ -11,7 +11,7 @@ import { FileModule } from './file/file.module';
       'mongodb+srv://muzadmin:2xM7OH9yBPlOK8pg@tracks.tnpcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     ),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'static')
+      rootPath: path.resolve(__dirname, 'static')
     }),
     SongModule,
     FileModule
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   try {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 4000;
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     await app.listen(port, () => console.log(`Server runs on port ${port}`));
